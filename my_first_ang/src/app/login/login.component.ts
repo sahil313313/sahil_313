241e5e4264820203cb92761ff47b6300920a6acc
sahil313313 <sahilshiraj3232@gmail.com>
2023-05-28 13:37:07 +0530

    new change

--- my_first_ang/src/app/login/login.component.ts
+++ my_first_ang/src/app/login/login.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  formTitle = '--LOGIN FORM--';
+  test:string= 'click';
+
+  submit() {
+    this.test = 'testing property';
+    this.formTitle = 'Login Form'
+  }
 
 }
