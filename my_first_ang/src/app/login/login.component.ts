8cb9138527b5e572663a94e26f0a933578580e62
sahil313313 <sahilshiraj3232@gmail.com>
2023-06-01 16:59:04 +0530

    new db

--- my_first_ang/src/app/login/login.component.ts
+++ my_first_ang/src/app/login/login.component.ts
@@ -14,10 +14,36 @@ export class LoginComponent {
     this.formTitle = 'Login Form'
   }
   amount : number =5000;
-  text:string ='sahil'
+  text:string ='sahil' 
 
   title(){
     return "LOG IN"
   }
+
+  //data binding 
+//.1. one way    2. two way
+
+// one way data binding types
+//1.interpolation {{}}
+//2. prperty binding []
+//3. event binding()
+
+// 1. interpolation{{}}
+myName = 'sahil';
+
+
+
+
+
+
+
+
+
+
+
+//2 . two way datat binding [()]
+myname1 = 'sahil';
+
 }
+
 
